69804b9dc8ef29d3ea1bde8b5c9913a66d9339f9
MarcCastellaPla <145784203+MarcCastellaPla@users.noreply.github.com>
2024-10-13 10:41:29 +0200

    Feature: Complete all exercices

--- index.js
+++ index.js
@@ -7,46 +7,71 @@ console.log(mainTitle)
 
 // Exercise 2: Changing HTML Content
 // TODO: Select all paragraph elements and change the content of the second paragraph to "This paragraph has been changed."
-let content = document.getElementById("paragraph");
 
-content.innerHTML = "<p>Updated content</p>";
+let paragraph = document.getElementsByClassName("paragraph");
+paragraph[1].innerHTML = "<p>Updated content</p>";
 
 // EXERCISE 3: Changing CSS Styles
 // Change the background color of the element with the ID 'content' to 'lightgreen'.
 
+let content = document.getElementById("content");
 content.style.backgroundColor = "lightgreen";
 
 // Exercise 4: Adding/Removing Classes
 // TODO: Add the "highlight" class to the first paragraph
 // TODO: Remove the "highlight" class from the first paragraph after 3 seconds
 
-let list_element = document.getElementById("list");
-console.log(list_element)
-list_element.classList.add("active");
-setTimeout(2000)
-list_element.remove()
+paragraph[0].classList.add("highlight");
+setTimeout(function () {
+    paragraph[0].classList.remove("highlight");
+}, 2000);
 
  // EXERCISE 5: Creating and Appending Elements
  // Create a new <button> element with the text 'Click Me'.
  // Append this button to the element with the ID 'content'.
 
+ let button = document.createElement("button");
+ button.textContent = "Click Me";
+ document.getElementById("content").appendChild(button);
 
  // EXERCISE 6: Removing Elements
  // Select the element with the ID 'result' and remove it from the DOM.
 
+ let result = document.getElementById("result");
+ result.remove();
 
  // EXERCISE 7: Handling Events
  // Add a 'click' event listener to the button created in Exercise 5.
  // When clicked, the button should alert "Button Clicked!".
 
+ button.addEventListener("click", function () {
+
+    alert("Button clicked!")
+
+ })
 
  // Exercise 8: Working with Input
  // Add an input event listener to the input field that displays the current input value in the result div.
 
+ let input = document.getElementById("inputField");
+ input.addEventListener("input", function () {
+
+    result.innerText = input.value;
+    console.log(input.value)
+
+})
 
  // Exercise 9: Toggling Visibility
  // Add a click event listener to the button with ID 'actionButton' that toggles the visibility of the content div.
 
+let actionButton = document.getElementById("actionButton");
+actionButton.addEventListener("click", function() {
+
+    content.classList.add("hidden");
+
+})
 
  // Exercise 10: Modifying Attributes
  // Select the input field and change its placeholder text to "Enter your name".
+
+ input.placeholder = "Marc"
